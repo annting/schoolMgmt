1d1b1076191fc83f318dc9f964d7eb20befc0406
annting <893208385@163.com>
2017-02-12 14:04:24 +0800

    controller demo

--- src/main/java/com/anting/controller/UserController.java
+++ src/main/java/com/anting/controller/UserController.java
@@ -1,6 +1,11 @@
 package com.anting.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.alibaba.fastjson.JSON;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by anting on 17/1/22.
@@ -8,6 +13,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
+
+    //@GetMapping("/hello")
+   // @RequestMapping(value = "/hello")
+    @PostMapping("/hello")
+    public String hello() {
+
+        List<String> ls = new ArrayList<>();
+        ls.add("123");
+
+        return JSON.toJSONString(ls);
+    }
 
 
 }
